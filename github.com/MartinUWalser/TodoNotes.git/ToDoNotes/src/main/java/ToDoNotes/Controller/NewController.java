584ed94cb0d7eb2d2249c2846d15d9635501a68f
walser <walserma@fim.uni-passau.de>
2015-06-18 10:13:55 +0200

    Querys Group update

--- github.com/MartinUWalser/TodoNotes.git/ToDoNotes/src/main/java/ToDoNotes/Controller/NewController.java
+++ github.com/MartinUWalser/TodoNotes.git/ToDoNotes/src/main/java/ToDoNotes/Controller/NewController.java
@@ -3,10 +3,12 @@ package ToDoNotes.Controller;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 
 import ToDoNotes.Bean.Group;
 import ToDoNotes.Bean.Note;
@@ -19,6 +21,7 @@ public class NewController implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	private Note note;
+	String groupName;
 	private ArrayList<Group> groupNamesList; 
 
 	public NewController(){
@@ -29,12 +32,18 @@ public class NewController implements Serializable {
 		java.sql.Date date = new java.sql.Date(utilDate.getTime());
 		NoteQuerys.insertNote(this.note.getTitle(), this.note.getDescription(), date, this.note.isVisible(),
 				this.note.isDone());
+		GroupQuerys.setIsInRelation(this.note, this.groupName);
 		return "<success>";
 	}
 	
 
 	@PostConstruct
 	public void initd(){
+		this.groupName = "";
+		Map<String,String> params =
+                FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+		this.groupName = params.get("group");
+        
 		this.note = new Note();
 		this.groupNamesList = GroupQuerys.getAllGroupNames();
 	}
@@ -53,5 +62,13 @@ public class NewController implements Serializable {
 
 	public void setNote(Note note) {
 		this.note = note;
+	}
+	
+	public String getGroupName() {
+		return groupName;
+	}
+
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
 	}
 }
